78e970e5b579039618a79fd3473d669f04abf2ea
Lenart Miklavič <miklavic.lenart@gmail.com>
2021-10-23 18:50:21 +0200

    Prva dokončana verzija modela

--- model.py
+++ model.py
@@ -14,32 +14,37 @@ class Dogodek:
         self.udelezenci = udelezenci
 
 
-def v_seznam(objekt):
+def v_seznam(objekt, ime_datoteke):
     """"Objekt 'udeleženec' zapiše v seznam v JSON datoteki."""
 
 # Ali datoteka s seznamom že obstaja? 
 
-    if os.path.isfile('seznam.json'):
-        with open('seznam.json', 'r', encoding='utf-8') as dat:
+    if os.path.isfile(ime_datoteke):
+        with open(ime_datoteke, 'r', encoding='utf-8') as dat:
             seznam = json.load(dat)
 
         seznam.append(objekt.__dict__)
 
-        with open('seznam.json', 'w', encoding='utf-8') as dat:
+        with open(ime_datoteke, 'w', encoding='utf-8') as dat:
             json.dump(seznam, dat, ensure_ascii=False, indent=4)
 
 # Če ne obstaja, odpri novo in objekt dodaj v prazen seznam.
 
     else:
-        with open('seznam.json', 'a', encoding='utf-8') as dat:
+        with open(ime_datoteke, 'a', encoding='utf-8') as dat:
             seznam = []
             seznam.append(objekt.__dict__)
             json.dump(seznam, dat, ensure_ascii=False, indent=4)
 
 
-def iz_seznama_v_dogodek(ime):
-    """Iz seznama udeležencev v dogodek zapiše udeleženca, ki je bil prisoten."""
-
-# Najprej v seznamu udeležencev poišče tistega, ki mu pripada ime. (Kaj pa če jih je več z istim imenom?)
-
-    pass
+def prisotni(manjkajoci):
+    """Vrne seznam vseh prosotnih, t.j. vseh v 'seznam.json' datoteki, ki jih ne navedemo v seznamu 'manjkajoci'."""
+    with open('seznam.json', 'r', encoding='utf-8') as dat:
+        udelezenci = json.load(dat)
+    
+    prisotni = []
+    for udelezenec in udelezenci:
+        if udelezenec['ime'] not in manjkajoci:
+            prisotni.append(udelezenec)
+    
+    return prisotni
