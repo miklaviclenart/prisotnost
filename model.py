212c122b58da49e3e5188428bf9fbfeb36762288
Lenart Miklavič <miklavic.lenart@gmail.com>
2021-10-23 10:02:55 +0200

    Dodal osnovno funkcijo

--- model.py
+++ model.py
@@ -1,5 +1,12 @@
+import json
+
 class Udelezenec:
     def __init__(self, ime, priimek) -> None:
         self.ime = ime
         self.priimek = priimek
-        
+
+
+def v_seznam(udelezenec):
+    """"Objekt 'udeleženec' zapiše v seznam v JSON datoteki."""
+    with open('seznam.json', 'a') as dat:
+        dat.write(json.dumps(udelezenec.__dict__))
